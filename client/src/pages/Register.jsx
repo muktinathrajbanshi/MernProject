0e2a4bb75a98ddadbf3cc8db63e0da7a394e6860
unknown <muktinathrajbanshi786@gmail.com>
2025-08-01 23:23:45 +0545

    let's begin with registration form design

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,3 +1,34 @@
 export const Register = () => {
-    return <h1>Hello Register Page</h1>;
-};
+    return(
+        <>
+        <section>
+            <main>
+                <div className="section-registration">
+                    <div className="container grid grid-two-cols">
+                        <div className="registration-image">
+                            <img 
+                            src="/images/registration.svg" 
+                            alt="regtration img"
+                            width="500"
+                            height="500"    
+                            />
+                        </div>
+                        {/* let's tackle registration form  */}
+                        <div className="registration-form">
+                            <h1 className="main-heading mb-3">registration form</h1>
+                            <br />
+
+                            <form>
+                                <div>
+                                    <label htmlFor="username">username</label>
+                                    <input type="text" name="username" value={} placeholder=""/>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </section>
+        </>
+    )
+   };
