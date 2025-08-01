a4f429fb5f57b9ebd173df397336d4c20dd0c577
unknown <muktinathrajbanshi786@gmail.com>
2025-08-01 19:07:38 +0545

    React Page Navigation with React Router Dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from 'react';
-
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { Service } from "./pages/Service";
+import { Register } from "./pages/Register";
+import { Login } from "./pages/Login";
 const App = () => {
   return (
-    <div>
-      <h1>Welcome to Vite</h1>
-    </div>
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+    </>
   )
-}
+};
 
 export default App;
