bb9a49974e6634ea06f5939537c817234e630c9c
unknown <muktinathrajbanshi786@gmail.com>
2025-08-01 23:57:00 +0545

    Complete Registration Page UI & stored Form data in React State Variable

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+
 export const Register = () => {
-    return(
+
+        const [user, setUser] = useState({
+            username:"muktinath",
+            email:"",
+            phone:"",
+            password: "",
+        });
+
+        // handling the input values 
+        const handleInput = (e) => {
+            console.log(e);
+            let name = e.target.name;
+            let value = e.target.value;
+
+            setUser({
+                ...user,
+                [name]:value,
+            });
+        };
+
+        // handling the form submission 
+        const handleSubmit = (e) => {
+            e.preventDefault();
+            console.log(user);
+            
+            alert(user);
+        };
+
+    return (
         <>
         <section>
             <main>
@@ -18,11 +48,61 @@ export const Register = () => {
                             <h1 className="main-heading mb-3">registration form</h1>
                             <br />
 
-                            <form>
+                            <form onSubmit={handleSubmit}>
                                 <div>
                                     <label htmlFor="username">username</label>
-                                    <input type="text" name="username" value={} placeholder=""/>
+                                    <input 
+                                    type="text" 
+                                    name="username" 
+                                    placeholder="username"
+                                    id="username"
+                                    required
+                                    autoComplete="off"
+                                    value={user.username}
+                                    onChange={handleInput}
+                                    />
                                 </div>
+                                <div>
+                                    <label htmlFor="email">email</label>
+                                    <input 
+                                    type="email" 
+                                    name="email" 
+                                    placeholder="enter your email"
+                                    id="email"
+                                    required
+                                    autoComplete="off"
+                                    value={user.email}
+                                    onChange={handleInput}
+                                    />
+                                </div>
+                                <div>
+                                    <label htmlFor="phone">phone</label>
+                                    <input 
+                                    type="number" 
+                                    name="phone" 
+                                    placeholder="phone"
+                                    id="phone"
+                                    required
+                                    autoComplete="off"
+                                     value={user.phone}
+                                    onChange={handleInput}
+                                    />
+                                </div>
+                                 <div>
+                                    <label htmlFor="password">password</label>
+                                    <input 
+                                    type="text" 
+                                    name="password" 
+                                    placeholder="password"
+                                    id="password"
+                                    required
+                                    autoComplete="off"
+                                     value={user.password}
+                                    onChange={handleInput}
+                                    />
+                                </div>
+                                <br />
+                                <button type="submit" className="btn btn-submit">Register Now</button>
                             </form>
                         </div>
                     </div>
@@ -30,5 +110,5 @@ export const Register = () => {
             </main>
         </section>
         </>
-    )
+    );
    };
