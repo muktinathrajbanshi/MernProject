260c08ab4228a75bea3021b3fc1c7ea1f4cd4bfd
unknown <muktinathrajbanshi786@gmail.com>
2025-08-01 21:27:44 +0545

    Building Navbar with React | Open Page without Reloading

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,3 +1,3 @@
 export const About = () => {
-    return <h1>Hello About Page</h1>
+    return <h1>Hello About Page</h1>;
 };
--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,3 +1,3 @@
 export const Contact = () => {
-    return <h1>Hello Contact Page</h1>
+    return <h1>Hello Contact Page</h1>;
 };
