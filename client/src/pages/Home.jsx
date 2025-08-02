adca7a7f9447c80c64d9d50d30d2e4a8d222c665
unknown <muktinathrajbanshi786@gmail.com>
2025-08-02 15:01:45 +0545

    My home page is started for design

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,20 @@
 export const Home = () => {
   return(
     <>
-      <h1>Hello Home Page</h1>
+     <main>
+      <section className="section-hero">
+        <div className="container grid grid-two-cols">
+          <p>We are the World Best IT Company</p>
+          <h1>Welcome to Muktinath Tech</h1>
+          <p>
+            Are you ready to take your business to the next level with 
+            cutting-edge IT solutions? Look no further! At Muktinath Tech,
+            we specialize in providing innovative IT services and solutions
+            tailored to meet your unique needs.
+          </p>
+        </div>
+      </section>
+     </main>
     </>
 );
 };
