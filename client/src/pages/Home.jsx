bc457ee4e1a9c1c9a6eea1fdf42738bcc3eadb17
unknown <muktinathrajbanshi786@gmail.com>
2025-08-02 21:04:18 +0545

    design is 70 % completed for home section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,14 +4,29 @@ export const Home = () => {
      <main>
       <section className="section-hero">
         <div className="container grid grid-two-cols">
-          <p>We are the World Best IT Company</p>
-          <h1>Welcome to Muktinath Tech</h1>
-          <p>
-            Are you ready to take your business to the next level with 
-            cutting-edge IT solutions? Look no further! At Muktinath Tech,
-            we specialize in providing innovative IT services and solutions
-            tailored to meet your unique needs.
-          </p>
+          <div className="hero-content">
+            <p>We are the World Best IT Company</p>
+            <h1>Welcome to Muktinath Tech</h1>
+            <p>
+              Are you ready to take your business to the next level with 
+              cutting-edge IT solutions? Look no further! At Muktinath Tech,
+              we specialize in providing innovative IT services and solutions
+              tailored to meet your unique needs.
+            </p>
+            <div className="btn btn-group">
+              <a href="/contact"><div className="btn">connect now</div></a>
+              <a href="/services"><div className="btn">learn more</div></a>
+            </div>
+          </div>
+          {/* hero images  */}
+          <div className="hero-image">
+            <img 
+            src="/images/hero.svg" 
+            alt="hero section img" 
+            width="400"
+            height="500"
+             />
+          </div>
         </div>
       </section>
      </main>
