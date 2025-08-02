765258327b831bbf7960301cb77c5da14694ada2
unknown <muktinathrajbanshi786@gmail.com>
2025-08-02 13:09:07 +0545

    Complete Login Page UI | Storing Login Form Data in React State

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,3 +1,84 @@
+import { useState } from "react";
+
 export const Login = () => {
-    return <h1>Hello Login Page</h1>;
+
+    const [user, setUser] = useState({
+        email: "",
+        password: "",
+    });
+
+    const handleInput = (e) => {
+        let name = e.target.name;
+        let value = e.target.value;
+
+
+        setUser({
+            ... user,
+            [name]: value,
+        });
+
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(user); 
+    };
+
+    return (
+        <>
+            <section>
+            <main>
+                <div className="section-registration">
+                    <div className="container grid grid-two-cols">
+                        <div className="registration-image">
+                            <img 
+                            src="/images/login.svg" 
+                            alt="login img"
+                            width="500"
+                            height="500"    
+                            />
+                        </div>
+                        {/* let's tackle registration form  */}
+                        <div className="registration-form">
+                            <h1 className="main-heading mb-3">registration form</h1>
+                            <br />
+
+                            <form onSubmit={handleSubmit}>
+                                <div>
+                                    <label htmlFor="email">email</label>
+                                    <input 
+                                    type="email" 
+                                    name="email" 
+                                    placeholder="enter your email"
+                                    id="email"
+                                    required
+                                    autoComplete="off"
+                                    value={user.email}
+                                    onChange={handleInput}
+                                    />
+                                </div>
+                                
+                                 <div>
+                                    <label htmlFor="password">password</label>
+                                    <input 
+                                    type="text" 
+                                    name="password" 
+                                    placeholder="password"
+                                    id="password"
+                                    required
+                                    autoComplete="off"
+                                     value={user.password}
+                                    onChange={handleInput}
+                                    />
+                                </div>
+                                <br />
+                                <button type="submit" className="btn btn-submit">Register Now</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </section>
+        </>
+    );
 };
