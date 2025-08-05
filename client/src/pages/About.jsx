f790cbeb4f5e8a6207f0b6c1f83a79849e33332b
unknown <muktinathrajbanshi786@gmail.com>
2025-08-05 20:19:59 +0545

    component create for reausability

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import { Analytics } from "../components/Analytics";
 
 export const About = () => {
     return (
@@ -51,26 +52,7 @@ export const About = () => {
       </section>
 
          {/* 2nd section  */}
-     <section className="section-analytics">
-        <div className="container grid grid-four-cols">
-          <div className="div1">
-            <h2>50+</h2>
-            <p>registered companies</p>
-          </div>
-          <div className="div1">
-            <h2>100,00+</h2>
-            <p>Happy Clients</p>
-          </div>
-          <div className="div1">
-            <h2>500+</h2>
-            <p>Well Known Developers</p>
-          </div>
-          <div className="div1">
-            <h2>24/7</h2>
-            <p>service</p>
-          </div>
-        </div>
-     </section>
+        <Analytics />
 
       {/* footer section start  */}
         <footer>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import { Analytics } from "../components/Analytics";
+
 export const Home = () => {
   return(
     <>
@@ -31,27 +33,8 @@ export const Home = () => {
       </section>
      </main>
 
-     {/* 2nd section  */}
-     <section className="section-analytics">
-        <div className="container grid grid-four-cols">
-          <div className="div1">
-            <h2>50+</h2>
-            <p>registered companies</p>
-          </div>
-          <div className="div1">
-            <h2>100,00+</h2>
-            <p>Happy Clients</p>
-          </div>
-          <div className="div1">
-            <h2>500+</h2>
-            <p>Well Known Developers</p>
-          </div>
-          <div className="div1">
-            <h2>24/7</h2>
-            <p>service</p>
-          </div>
-        </div>
-     </section>
+    {/* 2nd section  */}
+    <Analytics />
 
      {/* 3rd section  */}
       <section className="section-hero">
