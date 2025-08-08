13f77d3c338a539a309b26af67ec4da57853afaa
unknown <muktinathrajbanshi786@gmail.com>
2025-08-08 22:18:11 +0545

    JWT Token Verification Middleware + Creating Route to Get User Data from DB

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -81,6 +81,18 @@ const login = async (req, res) => {
 };
 
 
+// to send user data  User Logic 
+
+const user = async (req, res) => {
+    try {
+        const userData = req.user; 
+        console.log(userData); 
+        return res.status(200).json({ msg: userData });
+    } catch (error) {
+        console.log(`error from the user route ${error}`);
+        
+    }
+}
 
 
-module.exports = { home, register, login };
+module.exports = { home, register, login, user };
