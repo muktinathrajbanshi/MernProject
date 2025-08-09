2e5b043cfd3ab9c95c0a243336c833e0ecf4fbda
unknown <muktinathrajbanshi786@gmail.com>
2025-08-09 15:51:44 +0545

    Working Contact Form in MERN Stack | Store User Message to MongoDB Database

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import { useAuth } from "../store/auth";
 
+const defaultContactFormData = {
+    username: "",
+    email: "",
+    message: "",
+};
+
 export const Contact = () => {
 
-    const[contact, setContact] = useState({
-        username: "",
-        email: "",
-        message: "",
-    });
+    const[contact, setContact] = useState(defaultContactFormData);
 
     const [userData, setUserData] = useState(true);
 
@@ -34,11 +36,28 @@ export const Contact = () => {
         });
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
-        console.log(contact);
-        
+        try {
+            const response = await fetch("http://localhost:5000/api/form/contact", {
+                method: "POST",
+                headers:{
+                    "Content-Type":"application/json"
+                },
+                body:JSON.stringify(contact),
+            });
+
+            if(response.ok) {
+                setContact(defaultContactFormData);
+                const data = await response.json();
+                console.log(data);
+                alert("Message send successfully");
+                
+            }
+        } catch (error) {
+            console.log(error);
+        }
     };
 
 
