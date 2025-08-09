56faf8e609cda61dbd94a5347364042567c11718
unknown <muktinathrajbanshi786@gmail.com>
2025-08-09 20:11:03 +0545

    Creating Dynamic Cards by Fetching Backend Services Data with React

--- client/src/pages/Service.jsx
+++ client/src/pages/Service.jsx
@@ -1,3 +1,43 @@
+import { useAuth } from "../store/auth";
+
 export const Service = () => {
-    return <h1>Hello Service Page</h1>;
+
+    const { services } = useAuth();
+
+    return(
+        <>
+            <section className="section-services">
+                <div className="container">
+                    <h1 className="main-heading">Services</h1>
+                </div>
+                <div className="container grid grid-three-cols">
+
+                    {
+                        services.map((curElem, index) => {
+                            const { price, description, provider, service } = curElem;
+
+                            return (
+                             <div className="card" key={index}>
+                            <div className="card-img">
+                                <img 
+                                src="/images/services.svg" 
+                                alt="service img"
+                                width="500"
+                                />
+                            </div>
+                            <div className="card-details">
+                                <div className="grid grid-two-cols">
+                                    <p>{provider}</p>
+                                    <p>{price}</p>
+                                </div>
+                                <h2>{service}</h2>
+                                <p>{description}</p>
+                            </div>
+                          </div>
+                          );
+                        })}
+                </div>
+            </section>
+        </>
+    );
 };
--- client/src/store/auth.jsx
+++ client/src/store/auth.jsx
@@ -8,6 +8,7 @@ export const AuthContext = createContext();
 export const AuthProvider = ({children}) => {
     const [token, setToken] = useState(localStorage.getItem("token"));
     const [user, setUser] = useState("");
+    const [services, setServices] = useState("");
 
     const storeTokenInLS = (serverToken) => {
         return localStorage.setItem("token", serverToken);
@@ -37,20 +38,40 @@ export const AuthProvider = ({children}) => {
 
             if(response.ok){
                 const data = await response.json();
-                console.log("user data", data.userData);
+                // console.log("user data", data.userData);
                 setUser(data.userData);
             }
         } catch (error) {
             console.log("Error fetching user data");  
         }
-    }
+    };
+
+    // to fetch the services data from the database 
+    const getServices = async() => {
+        try {
+            const response = await fetch("http://localhost:5000/api/data/service", {
+                method: "GET",
+            });
+
+            if(response.ok){
+                const data = await response.json();
+                console.log(data.msg);
+                setServices(data.msg);
+                
+            }
+        } catch (error) {
+            console.log(`services frontend error: ${error}`);
+            
+        }
+    };
 
     useEffect(() => {
+        getServices();
         userAuthentication();
     }, []);
 
     return (
-    <AuthContext.Provider value={{ isLoggedIn, storeTokenInLS, LogoutUser, user }}>
+    <AuthContext.Provider value={{ isLoggedIn, storeTokenInLS, LogoutUser, user, services }}>
         {children}
     </AuthContext.Provider>
     );
