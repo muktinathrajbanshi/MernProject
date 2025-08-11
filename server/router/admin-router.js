70225167859d51c6240b8e329b74678d24995286
unknown <muktinathrajbanshi786@gmail.com>
2025-08-11 15:20:48 +0545

    Secure our Admin Route with JWT Verification and Middleware Integration in MERN Stack

--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const adminController = require("../controllers/admin-controller");
-const router = express.Router();
+const authMiddleware = require("../middlewares/auth-middleware");
+const router = require("./auth-router");
 
 
-router.route("/users").get(adminController.getAllUsers);
-router.route("/contacts").get(adminController.getAllContacts);
+router.route("/users").get(authMiddleware, adminController.getAllUsers);
+router.route("/contacts").get(authMiddleware, adminController.getAllContacts);
 
 
 module.exports = router;
