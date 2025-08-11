dafb297d850d1aba6074fcdc6a6fc116ac0af8d3
unknown <muktinathrajbanshi786@gmail.com>
2025-08-11 22:42:39 +0545

    CRUD in MERN Stack: Admin User Page with React JS by Fetching Data from Back-End

--- client/src/store/auth.jsx
+++ client/src/store/auth.jsx
@@ -9,6 +9,7 @@ export const AuthProvider = ({children}) => {
     const [token, setToken] = useState(localStorage.getItem("token"));
     const [user, setUser] = useState("");
     const [services, setServices] = useState("");
+    const authorizationToken = `Bearer ${token}`;
 
     const storeTokenInLS = (serverToken) => {
         setToken(serverToken);
@@ -33,7 +34,7 @@ export const AuthProvider = ({children}) => {
             const response = await fetch("http://localhost:5000/api/auth/user", {
                 method:"GET",
                 headers: {
-                    Authorization: `Bearer ${token}`,
+                    Authorization: authorizationToken,
                 },
             });
 
@@ -72,7 +73,7 @@ export const AuthProvider = ({children}) => {
     }, []);
 
     return (
-    <AuthContext.Provider value={{ isLoggedIn, storeTokenInLS, LogoutUser, user, services }}>
+    <AuthContext.Provider value={{ isLoggedIn, storeTokenInLS, LogoutUser, user, services, authorizationToken }}>
         {children}
     </AuthContext.Provider>
     );
