4a6bad70f4a73db013615b9a4601ededeb0159a3
unknown <muktinathrajbanshi786@gmail.com>
2025-08-12 21:21:43 +0545

    Display all Contact Data in our Admin Contact Page | MERN Stack

--- client/src/pages/Admin-Contacts.jsx
+++ client/src/pages/Admin-Contacts.jsx
@@ -1,7 +1,56 @@
+import { useEffect } from "react";
+import { useAuth } from "../store/auth";
+import { useState } from "react";
+
 export const AdminContacts = () => {
+    const [contactData, setContactData] = useState([]);
+    const { authorizationToken } = useAuth();
+
+    const getContactsData = async () => {
+        try {
+            const response = await fetch("http://localhost:5000/api/admin/contacts", {
+                method:"GET",
+                headers: {
+                    Authorization: authorizationToken,
+                },
+            });
+            const data = await response.json();
+            console.log("contact data: ", data);
+            if(response.ok){
+                setContactData(data);                
+            }
+            
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+
+    useEffect(() => {
+        getContactsData();
+    }, []);
+
     return (
         <>
-            <h1>Admin Contacts Panel</h1>
+           <section className="admin-contacts-section">
+            <h1>Admin Contact Data</h1>
+
+            <div className="container admin-users">
+                {
+                    contactData.map((curContactData, index) => {
+                        const { username, email, message} = curContactData;
+                        return (
+                            <div key={index}>
+                                <p>{username}</p>
+                                <p>{email}</p>
+                                <p>{message}</p>
+                                <button className="btn">delete</button>
+                            </div>
+                        );
+                    })
+                }
+            </div>
+           </section>
         </>
     );
 };
